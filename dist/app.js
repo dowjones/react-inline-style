faee6956274bc580343f0fcf80aebb6ff5b16757
Feram <feram@feram.co>
2016-08-30 19:19:47 +0000

    Fix typo inadvertantly

--- dist/app.js
+++ dist/app.js
@@ -8802,7 +8802,7 @@ var ReactDOMIDOperations = {
     ) : invariant(!INVALID_PROPERTY_ERRORS.hasOwnProperty(name)));
 
     // If we're updating to null or undefined, we should remove the property
-    // from the DOM node instead of inadvertantly setting to a string. This
+    // from the DOM node instead of inadvertently setting to a string. This
     // brings us in line with the same behavior we have on initial render.
     if (value != null) {
       DOMPropertyOperations.setValueForProperty(node, name, value);
